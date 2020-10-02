a71e4b98573f41692935520a43d83b88755cff2a
nebojsadj <djordjevicn87@gmail.com>
2020-10-02 08:26:04 +0200

    added new file

--- main.js
+++ main.js
@@ -113,33 +113,4 @@ window.addEventListener('load', run);
                         repeat();
                     };
 
-        function zagarantovanaSuma() {
-            osvojiliSte.style.display = 'block';
-            osvojenaSuma.style.display = 'block';
-            ukupnoPoena.style.display = 'block';
-            ukupnoPoena.innerHTML = quiz.score + ' poena';
-          if (brojac >= 5) {
-              osvojiliSte.style.display = 'block';
-              osvojenaSuma.innerHTML = '3.000' +' rsd';
-              osvojenaSuma.style.display = 'block';
-              ukupnoPoena.innerHTML = quiz.score + ' poena';
-              ukupnoPoena.style.display = 'block';
-          }else {
-              osvojiliSte.style.display = 'block';
-              osvojenaSuma.innerHTML = '0' +' rsd';
-              osvojenaSuma.style.display = 'block';
-              ukupnoPoena.style.display = 'block';
-              ukupnoPoena.innerHTML = quiz.score + ' poena';
-          }
-
-          if (brojac >= 10) {
-            osvojiliSte.style.display = 'block';
-            osvojenaSuma.innerHTML = '96.000' +' rsd';
-            osvojenaSuma.style.display = 'block';
-            ukupnoPoena.style.display = 'block';
-            ukupnoPoena.innerHTML = quiz.score + ' poena';
-          }
-        };
   };
-
-  
